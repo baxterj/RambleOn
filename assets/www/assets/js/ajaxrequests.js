0cf94a22c536b773da01a0f4b8ac8803f06815b1
unknown <jkb25@.ad.susx.ac.uk>
2012-12-11 13:02:11 +0000

    new holder pages, start of routes list - still need sorting by done my fave etc

--- assets/www/assets/js/ajaxrequests.js
+++ assets/www/assets/js/ajaxrequests.js
@@ -1,5 +1,5 @@
 var apiUrl = 'http://www.rambleonline.com/api/v1/'
-
+var routesListType = 'my' //my, favourite, done
 
 
 function sendLogin(data, messageTarget){
@@ -20,7 +20,7 @@ function sendRegister(data, messageTarget){
 	sendAjax(data, messageTarget, successLoginRegister, 'register', 'POST', false)
 }
 
-function successLoginRegister(data){
+function successLoginRegister(data, messageTarget){
 	window.localStorage.setItem("apikey", data.key)
 	window.localStorage.setItem("user", data.user)
 	$.mobile.changePage("home.html")
@@ -50,6 +50,31 @@ function redirectLoggedIn(){
 }
 
 
+function getMyRoutesItems(messageTarget, listType){
+	routesListType = listType
+	sendAjax(null, messageTarget, successRoutesList, 'myroutes', 'GET', true)
+}
+
+function successRoutesList(data, messageTarget){
+	var html = ''
+	if(data.meta.total_count < 1){
+		messageTarget.html('No routes found for user: ' + window.localStorage.getItem("user"))
+	}else{
+		for(var i = 0; i < data.objects.length; i++){
+			//html += '<li><a href="index.html">route' + i + '</a></li>'
+			html += createRouteListItem(data.objects[i])
+		}
+	}
+	$('#myRoutesList').html(html)
+	$('#myRoutesList').listview('refresh')
+}
+
+function createRouteListItem(route){
+	var html = '<li><a href="route.html?id=' + route.id + '">' + route.name + '</a></li>'
+	return html
+}
+
+
 //Generic function for sending ajax requests, pass error message display target
 //and function for what to do on success
 function sendAjax(data, messageTarget, successFunc, apiLocation, reqType, useAuth){
@@ -69,7 +94,7 @@ function sendAjax(data, messageTarget, successFunc, apiLocation, reqType, useAut
 		crossDomain: true,
 		processData: false,
 		success: function(data, status, jqXHR) {
-			successFunc(data)
+			successFunc(data, messageTarget)
 		},
 		error: function(jqXHR, textStatus, errorThrown) {
 			messageTarget.html(jQuery.parseJSON(jqXHR.responseText).error_message)
--- assets/www/assets/js/jquery-mobile-config.js
+++ assets/www/assets/js/jquery-mobile-config.js
@@ -3,7 +3,7 @@ $(document).on("mobileinit", function() {
 	$.support.cors = true;
 	$.mobile.allowCrossDomainPages = true;
 	$.mobile.buttonMarkup.hoverDelay = 100;
-	$.mobile.defaultPageTransition = 'slide';
+	$.mobile.defaultPageTransition = 'none';
 	//$.mobile.ajaxEnabled = false;
 	//$.mobile.pushStateEnabled = false;
 	$.mobile.selectmenu.prototype.options.nativeMenu = false;
